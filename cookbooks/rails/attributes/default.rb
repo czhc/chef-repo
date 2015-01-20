61818cb180bd2617062b41780588b0eed7a2de43
Cedric ZH <cedrizhchan@gmail.com>
2015-01-20 14:57:58 +0800

    set app root for cap default

---
+++ cookbooks/rails/attributes/default.rb
@@ -0,0 +1,10 @@
+default["rails"]["applications_root"] = "/var/www"
+default["rbenv"]["binaries_url"] = "http://binaries.intercityup.com/ruby/ubuntu"
+default["rbenv"]["available_binaries"] = %w(1.9.3-p547 2.0.0-p481 2.1.1 2.1.2 2.1.3)
+
+case node["platform_family"]
+when "debian"
+  if node["platform"] == "ubuntu" && node["platform_version"] == "14.04"
+    default["nginx"]["pid"] = "/run/nginx.pid"
+  end
+end
