577e8db2b3e7b4ce6d642eecdfab22f879f49109
czhc <cedriczhchan@gmail.com>
2017-04-17 00:52:50 +0800

    Patch weird message thrown from wrapping node args

--- vendor/cookbooks/nvm/recipes/node.rb
+++ vendor/cookbooks/nvm/recipes/node.rb
@@ -1,8 +1,8 @@
 # install nvm
 include_recipe 'nvm'
 
-# install node.js v0.10.5
-nvm_install node[:nvm] do
+# install node.js v0.10.5s
+nvm_install(node[:nvm][:version]) do
   from_source node[:nvm][:from_source] || false
   alias_as_default node[:nvm][:alias_as_default] || true
   action :create
