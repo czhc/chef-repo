2ccdd182fdf586ba8b6a2ac65522e3549cb65772
czhc <cedriczhchan@gmail.com>
2018-04-26 17:51:31 +0800

    Update rbenv dep, remove explicit dep in cheffile

--- vendor/cookbooks/rails/metadata.rb
+++ vendor/cookbooks/rails/metadata.rb
@@ -5,7 +5,7 @@ license          "MIT"
 description      "Installs/Configures rails"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.4.1"
-depends "rbenv", "~> 1.9.0"
+depends "rbenv", "1.9.0"
 depends "sudo", "> 1.2.0"
 depends "database"
 depends "nginx"
